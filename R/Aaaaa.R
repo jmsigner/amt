59e472692a5f358f30fdf93a24c3a916adc7b2ee
jmsigner <jmsigner@gmail.com>
2017-07-13 06:34:57 +0200

    fixd documentation

--- R/Aaaaa.R
+++ R/Aaaaa.R
@@ -1,9 +1,9 @@
 #' @importFrom dplyr arrange arrange_ filter filter_ group_by group_by_ select select_ summarize summarize_ summarise summarise_
 #' @importFrom dplyr distinct distinct_ ungroup
-#' @importFrom dplyr mutate mutate_ %>%
+#' @importFrom dplyr mutate %>%
 #' @importFrom dplyr bind_rows
-#' @importFrom dplyr select_vars_ select_vars
-#' @importFrom tidyr nest nest_ unnest unnest_
+#' @importFrom dplyr  select_vars
+#' @importFrom tidyr nest unnest
 #' @importFrom purrr map
 #' @importFrom lazyeval lazy_dots lazy
 #' @importFrom tibble as_data_frame data_frame tibble tribble
@@ -11,7 +11,8 @@
 #' @importFrom grDevices adjustcolor
 #' @importFrom graphics legend
 #' @importFrom methods is
-#' @importFrom stats coef dgamma median na.omit qgamma quantile runif
+#' @importFrom stats coef dgamma median na.omit qgamma quantile runif sd
+#' @importFrom graphics plot
 #' @importFrom utils data head tail
 #' @importFrom lubridate hours minutes seconds
 #' @import survival tidyverse
