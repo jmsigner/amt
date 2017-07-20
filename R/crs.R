0449a0c4a01b47068edb83c6475b9b6c601b9039
jmsigner <jmsigner@gmail.com>
2017-07-20 05:51:50 +0200

    crs for hr

--- R/crs.R
+++ R/crs.R
@@ -15,19 +15,17 @@ get_crs <- function(x, ...) {
 }
 
 #' @export
-get_crs.track_xy <- function(x, ...) {
-  attr(x, "crs", ...)
-}
-
-#' @export
-get_crs.steps <- function(x, ...) {
-  attr(x, "crs", ...)
-}
-
-#' @export
 #' @rdname crs
 has_crs <- function(x, ...) {
   UseMethod("has_crs", x)
+}
+
+
+# track -------------------------------------------------------------------
+
+#' @export
+get_crs.track_xy <- function(x, ...) {
+  attr(x, "crs", ...)
 }
 
 #' @export
@@ -35,7 +33,31 @@ has_crs.track_xy <- function(x, ...) {
   !is.null(attr(x, "crs", ...))
 }
 
+
+# steps -------------------------------------------------------------------
+
+#' @export
+get_crs.steps <- function(x, ...) {
+  attr(x, "crs", ...)
+}
+
 #' @export
 has_crs.steps <- function(x, ...) {
   !is.null(attr(x, "crs", ...))
 }
+
+
+# hr ----------------------------------------------------------------------
+
+
+#' @export
+get_crs.hr <- function(x, ...) {
+  attr(x, "crs", ...)
+  sp::proj4string(hr_isopleths(x))
+}
+
+#' @export
+has_crs.hr <- function(x, ...) {
+  !is.na(get_crs(x))
+}
+
