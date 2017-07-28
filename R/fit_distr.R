dccf12dbaa35c89b726f483ff05922608eb50e0c
jmsigner <jmsigner@gmail.com>
2017-07-28 06:36:58 +0200

    rm lower for fitdistr, to get se

--- R/fit_distr.R
+++ R/fit_distr.R
@@ -30,7 +30,7 @@ fit_sl_dist_base <- function(x, na.rm = TRUE, distr = "gamma", ...) {
     stop("0 length steps are not possible, consider adding a random error")
   }
 
-  fitdistrplus::fitdist(x, distr, lower = c(0, 0), keepdata = FALSE)
+  fitdistrplus::fitdist(x, distr, keepdata = FALSE)
 
 }
 
