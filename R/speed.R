afba152ac19e764611dd4b7356a8e1f2f06d2d34
jmsigner <jmsigner@gmail.com>
2017-08-08 07:26:58 +0200

    added speed

---
+++ R/speed.R
@@ -0,0 +1,26 @@
+#' Speed
+#'
+#' Obtain the speed of a track.
+#'
+#' @param x A `track_xyt`.
+#' @param append_na `[logical(1)=TRUE]` \cr Should an `NA` be appended at the end.
+#' @return `[numeric]` \cr The speed in `m/s`.
+#' @examples
+#' data(deer)
+#' speed(deer)
+#'
+
+speed <- function(x, ...) {
+  UseMethod("speed", x)
+}
+
+speed.track_xyt <- function(x, append_na = TRUE, ...) {
+  stps <- suppressWarnings(steps(x))
+  s <- stps$sl_ / as.numeric(stps$dt_, units = "secs")
+  if (append_na) {
+    c(s, NA)
+  } else {
+    s
+  }
+
+}
