e87195bb330280054d04f333ed927600a741bcb9
johannes <jmsigner@gmail.com>
2018-01-06 18:45:01 +0100

    new movement kernel

--- R/movement_kernel.R
+++ R/movement_kernel.R
@@ -1,29 +1,19 @@
+#' @param scale,shape `[numeric](1)\cr` Scale and scale parameter of the gamma distribution of step lengths.
 #' @param quant A numeric scalar, quantile of the step-length distribution that is
 #'   the maximum movement distance.
-#' @param adjust A logical scalar, indicating whether or not step length
-#'   parameters should be adjusted or not. Note, parameter adjustment is only
-#'   possilbe, if movement related covariates were included in the model (e.g.,
-#'   step length and the log thereof).
 #' @details  **`movement_kernel()`:** calculates a movement kernel from a fitted
 #'   (i)SSF. The method is currently only implemented for the gamma
 #'   distribution.
 #' @export
 #' @rdname sim_ud
-movement_kernel <- function(fit, resources, quant = 0.99, adjust = FALSE) {
-  if (sl_distr(fit) == "gamma") {
-    params <- if (adjust) adjust_params(fit) else sl_params(fit)[, "est"]
-
-    dist <- ceiling(qgamma(quant, scale = params["scale"], shape = params["shape"]))
-
-    mk <- mk_base(dist, raster::res(resources)[1])
-    mk <- mk_gamma(mk, shape= params["shape"], scale = params["scale"])
-    attributes(mk)$abt <- list(dist = sl_distr(fit), params = params,
+movement_kernel <- function(scale, shape, template, quant = 0.99) {
+    dist <- ceiling(qgamma(quant, scale = scale, shape = shape))
+    mk <- mk_base(dist, raster::res(template)[1])
+    mk <- mk_gamma(mk, shape= shape, scale = scale)
+    attributes(mk)$abt <- list(scale = scale, shape = shape,
                                adjust = adjust, dist = dist,
                                quant = quant)
     mk
-  } else {
-    stop("not yet implemented")
-  }
 }
 
 mk_base <- function(r, res) {
