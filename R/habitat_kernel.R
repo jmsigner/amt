e661ad6ca116de8c72042844842c5559707546ea
johannes <jmsigner@gmail.com>
2018-01-06 20:03:21 +0100

    adapted

--- R/habitat_kernel.R
+++ R/habitat_kernel.R
@@ -1,21 +1,18 @@
 #' @param exp A logical scalar, indicating whether or not the resulting habitat kernel should be exponentiated. This is usually `TRUE`.
-#' @param coef `[numeric]` \cr Vector with coeffiecients, not yet implemented.
+#' @param coef `[list]` \cr Vector with coeffiecients, not yet implemented.
 #' @rdname sim_ud
 #' @export
-habitat_kernel <- function(fit, resources, exp = TRUE, coef = NULL) {
+habitat_kernel <- function(coef, resources, exp = TRUE) {
 
-  vars <- intersect(
-    names(resources),
-    names(coef(fit)))
-
-  if (!length(vars)) {
+  if (!all(names(coef) %in% names(resources))) {
     stop("Coef and covars share no common arg.")
   }
 
   hk <- raster::raster(resources)
   hk <- raster::setValues(hk, 0)
-  for (i in vars) {
-    hk <- hk + resources[[i]] * coef(fit)[i]
+
+  for (i in names(coef)) {
+    hk <- hk + resources[[i]] * coef[[i]]
   }
   if (exp) {
     hk <- exp(hk)
