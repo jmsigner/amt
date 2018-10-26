24ac2202387e30ec949c2b49169bd334585442ea
johannes <jmsigner@gmail.com>
2018-10-26 07:32:44 +0200

    fixed progress

--- R/od.R
+++ R/od.R
@@ -1,6 +1,6 @@
-#' Occurence Distribution
+#' Occurrence Distribution
 #'
-#' `od` is a wrapper around `ctmm::occurrence`. See `help(ctmm::occurrence)` for more details. `rolling_od` estimates occurences distributions for a subset of a track.
+#' `od` is a wrapper around `ctmm::occurrence`. See `help(ctmm::occurrence)` for more details. `rolling_od` estimates occurrence distributions for a subset of a track.
 #'
 #'
 #' @template track_xyt
@@ -9,6 +9,7 @@
 #' @param res.space `[numeric(1)=10]` \cr Number of grid point along each axis, relative to the average diffusion (per median timestep) from a stationary point. See also `help(ctmm::occurrence)`.
 #' @param res.time `[numeric(1)=10]` \cr Number of temproal grid points per median timestep.
 #' @param n.points `[numeric(1)=5]` \cr This argument is only relevant for `rolling_od` and specifies the window size for the od estimation.
+#' @param show.progress `[logical(1)=TRUE]` \cr Indicates if a progress bar is used.
 #' @template dots_none
 #' @references Fleming, C. H., Fagan, W. F., Mueller, T., Olson, K. A., Leimgruber, P., & Calabrese, J. M. (2016). Estimating where and how animals travel: an optimal framework for path reconstruction from autocorrelated tracking data. Ecology.
 #' @name od
@@ -22,11 +23,11 @@ rolling_od <- function(x, ...) {
 #' @rdname od
 rolling_od.track_xyt <- function(
   x, trast, model = "bm", res.space = 10, res.time = 10,
-  n.points = 5, ...) {
+  n.points = 5, show.progress = TRUE, ...) {
   res <- list()
-  pb <- utils::txtProgressBar(style = 3, max = nrow(x), min = n.points + 1)
+  if (show.progress) pb <- utils::txtProgressBar(style = 3, max = nrow(x), min = n.points + 1)
   for (i in (n.points + 1):nrow(x)) {
-    utils::setTxtProgressBar(pb, i)
+    if (show.progress) utils::setTxtProgressBar(pb, i)
     j <- i - n.points
     suppressWarnings(res[[j]] <- od(x[j:i, ], trast = trast, model = model,
                                     res.space = res.space, res.time = res.time))
