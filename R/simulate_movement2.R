20d6c329cbf30880029c50dfb008ac988369a612
Johannes Signer <jmsigner@gmail.com>
2020-01-07 12:27:48 +0100

    new simulation methods

---
+++ R/simulate_movement2.R
@@ -0,0 +1,394 @@
+# Functions ---------------------------------------------------------------
+
+get_angle <- function(xy, dir = 0, take_cosine = TRUE) {
+  ta <- atan2(xy[, 2], xy[, 1])
+  ta <- ta - (pi/2) + dir
+  ta <- ifelse(ta < -pi, ta + 2 * pi, ta)
+  ta <- ifelse(ta > pi, ta - 2 * pi, ta)
+  if (take_cosine) cos(ta) else ta
+}
+
+prep_and_check_simulations_to_rcpp <- function(
+  formula, coefs, habitat, other_vars = NULL, start, max.dist,
+  init.dir = amt::as_rad(45), standardize = TRUE, stop, n = 1
+) {
+
+  ##
+  # formula <- f
+  # coefs <- coefs
+  # habitat <- h
+  # other_vars = NULL
+  # start <- c(100, 100)
+  # max.dist <- 10
+  ##
+
+  checkmate::assert_formula(formula)
+  checkmate::assert_named(coefs)
+  checkmate::assert_numeric(start, len = 2)
+  checkmate::assert_numeric(max.dist, lower = 0, len = 1)
+
+  # check that start is within landscape
+  if (!(start[1] >= raster::xmin(habitat) & start[1] <= raster::xmax(habitat) &
+    start[2] >= raster::ymin(habitat) & start[2] <= raster::ymax(habitat))) {
+    stop("Start position is not within landscape")
+  }
+
+  # prep terms
+  trms <- attr(terms(formula), "term.labels")
+  all_terms <- unique(do.call(c, strsplit(trms, ":")))
+
+  data_names <- c("sl_", "log_sl_", "cos_ta_")
+  other_vars_indicator <- c(0, 0)
+
+  # habitat
+  if (is.null(habitat)) {
+    stop("habitat mask is needed.")
+  }
+
+  hab_end <- sapply(strsplit(all_terms[grepl(pattern = "_end$", all_terms)],
+                             "_end"), "[", 1)
+  hab_start <- sapply(strsplit(all_terms[grepl(pattern = "_start$", all_terms)],
+                               "_start"), "[", 1)
+  data_names <- c(data_names, paste0(names(habitat), "_start"),
+                  paste0(names(habitat), "_end"))
+  res <- raster::res(habitat)[1]
+
+  # other
+  if (!is.null(other_vars)) {
+    data_names <- c(data_names, colnames(other_vars))
+    other_vars_indicator[2] <- 1
+    if (!is(other_vars, "matrix")) {
+      other_vars <- as.matrix(other_vars)
+    }
+  } else {
+    other_vars <- matrix(NA, nrow = n, 1)
+  }
+
+  first_order <- sapply(strsplit(trms, ":"), "[", 1)
+  int_with <- sapply(strsplit(trms, ":"), "[", 2)
+
+  if (!all(first_order %in% data_names)) {
+    stop(paste0(
+      "Some first order terms (",
+      paste(first_order[!first_order %in% data_names], collapse = ",") ,
+      ") not in coefficients. Did you forget *term*_start or *term*_end?" ))
+  }
+
+  first_order_terms <- match(first_order, data_names)
+  second_order_terms <- match(int_with, data_names, nomatch = 0)
+
+  # Dispersal kernel stencil, in cells
+  dist_in_cells <- ceiling(max.dist / res)
+
+  dk <- expand.grid(x = -dist_in_cells:dist_in_cells,
+                    y = -dist_in_cells:dist_in_cells)
+  dk[, "sl_"] <- sqrt(dk[, "x"]^2 + dk[, "y"]^2) * res
+  dk <- dk[dk[, "sl_"] < dist_in_cells * res, ]
+  dk[, "log_sl_"] <- log(dk[, "sl_"])
+  dk[, "cos_ta_"] <- get_angle_cpp2(as.matrix(dk[, 1:2]), dir = init.dir)
+  dk <- as.matrix(dk)
+
+
+  nc <- ncol(habitat)
+  nr <- nrow(habitat)
+  hab1 <- (raster::getValues(habitat))
+  other_vars_indicator[1] <- 1
+
+  # Start as cells
+  start[1] <- raster::colFromX(habitat, start[1]) - 1
+  start[2] <- nr - raster::rowFromY(habitat, start[2]) + 1
+
+  list(
+    start = start,
+    nc = nc, nr = nr,
+    res = res,
+    dk = dk, coefs = coefs,
+    init_dir = init.dir, standardize = standardize,
+    first_order_terms = first_order_terms - 1,
+    second_order_terms = second_order_terms - 1,
+    hab = hab1,
+    other_covars = other_vars,
+    other_covars_indicator = other_vars_indicator,
+    data_names = data_names,
+    stop = stop
+  )
+}
+
+dispersal_kernel_1 <- function(
+  formula, coefs, habitat = NULL, other_vars = NULL, start, max.dist,
+  init.dir = amt::as_rad(45), standardize = TRUE, raster = TRUE, stop = 0) {
+
+
+  ##
+  # formula = f
+  # coefs = coefs
+  # habitat = h
+  # other_vars = data.frame(tod = 1)
+  # start = start
+  # max.dist = 150
+  # init.dir = amt::as_rad(180)
+  # raster = TRUE
+  # standardize = TRUE
+  # stop= 0
+  # ##
+
+
+  # Prepare params
+  p <- prep_and_check_simulations_to_rcpp(
+    formula, coefs, habitat, other_vars, start, max.dist,
+    init.dir, standardize, stop
+  )
+
+  k <- dispersal_kernel(
+    cur_x = p$start[1], cur_y = p$start[2], nc = p$nc, nr = p$nr,
+    dk = p$dk, coefs = p$coefs,
+    standardize = p$standardize,
+    first_order_terms = p$first_order_terms,
+    second_order_terms = p$second_order_terms,
+    hab = p$hab,
+    other_covars = p$other_covars,
+    other_covars_indicator = p$other_covars_indicator,
+    stop = p$stop
+  )
+
+  # Return
+  kernel <- if (raster) {
+    raster::rasterFromXYZ(cbind(
+      k[, 1] * p$res,
+      k[, 2] * p$res,
+      k[, 3]))
+  } else {
+    cbind(
+      x = k[, 1] * p$res + start[1],
+      y = k[, 2] * p$res + start[2],
+      sel = k[, 3]
+    )
+  }
+
+  out <- list(
+    formula = formula,
+    coefs = coefs,
+    habitat = habitat,
+    other_vars = other_vars,
+    start = start,
+    max.dist = max.dist,
+    init.dir = init.dir,
+    standardize = standardize,
+    raster = raster,
+    stop = stop,
+    prep_dk = p,
+    dispersal_kernel = kernel
+  )
+  class(out) <- c("dispersal_kernel", class(out))
+  out
+}
+
+
+simulate_xy_R1 <- function(obj, n = 100, other_vars = NULL) {
+
+   if (!is(obj, "dispersal_kernel")) {
+     stop("obj is no dispersal kernel")
+   }
+   p <- obj$prep_dk
+   if (is.null(obj$other_vars)) {
+     p$other_covars <- matrix(NA, nrow = n, ncol = 1)
+   }
+
+   x <- p$start[1]
+   y <- p$start[2]
+
+   out <- data.frame(
+     x1 = rep(NA, n),
+     y1 = NA,
+     x2 = NA,
+     y2 = NA,
+     sl_ = NA,
+     ta_ = NA
+   )
+
+   dk <- p$dk
+
+   for (i in 1:n) {
+     out$x1[i] <- x
+     out$y1[i] <- y
+     k <- dispersal_kernel(
+       cur_x = x, cur_y = y, nc = p$nc, nr = p$nr,
+       dk = p$dk, coefs = p$coefs,
+       standardize = p$standardize,
+       first_order_terms = p$first_order_terms,
+       second_order_terms = p$second_order_terms,
+       hab = p$hab,
+       other_covars = p$other_covars,
+       other_covars_indicator = p$other_covars_indicator,
+       stop = p$stop
+     )
+
+     next_pixel <- sample.int(nrow(p$dk), size = 1, prob = k[, 3])
+     x <- k[next_pixel, 1]
+     y <- k[next_pixel, 2]
+
+     ta <- atan2_north_cpp(y -  out[i, "y1"], x - out[i, "x1"])
+     dk[, "cos_ta_"] <- get_angle_cpp2(dk[, 1:2], dir = ta)
+     out$x2[i] <- x
+     out$y2[i] <- y
+     out$sl_[i] <- p$dk[next_pixel, "sl_"]
+     out$ta_[i] <- ta
+   }
+   out
+ }
+
+simulate_ud <- function(obj, n = 1e3, other_vars = NULL) {
+
+   if (!is(obj, "dispersal_kernel")) {
+     stop("obj is no dispersal kernel")
+   }
+   p <- obj$prep_dk
+   if (is.null(obj$other_vars)) {
+     p$other_covars <- matrix(NA, nrow = n, ncol = 1)
+   }
+
+   ud <- obj$habitat
+
+   x <- p$start[1]
+   y <- p$start[2]
+   dk <- p$dk
+
+   cells <- rep(NA, n)
+
+   for (i in 1:n) {
+     k <- dispersal_kernel(
+       cur_x = x, cur_y = y, nc = p$nc, nr = p$nr,
+       dk = p$dk, coefs = p$coefs,
+       standardize = p$standardize,
+       first_order_terms = p$first_order_terms,
+       second_order_terms = p$second_order_terms,
+       hab = p$hab,
+       other_covars = p$other_covars,
+       other_covars_indicator = p$other_covars_indicator,
+       stop = p$stop
+     )
+
+     next_pixel <- sample.int(nrow(p$dk), size = 1, prob = k[, 3])
+     xn <- k[next_pixel, 1]
+     yn <- k[next_pixel, 2]
+
+     cells[i] <- cellFromXY(ud, cbind(xn, yn))
+     ta <- atan2_north_cpp(yn -  y, xn - x)
+     dk[, "cos_ta_"] <- get_angle_cpp2(dk[, 1:2], dir = ta)
+     x <- xn
+     y <- yn
+   }
+
+   cl <- table(cells)
+   ud[as.numeric(names(cl))] <- cl
+   ud
+ }
+
+
+# #### OLD
+# simulate_xy <- function(x, n = 100, other_vars = NULL) {
+#
+#   if (!is(x, "dispersal_kernel")) {
+#     stop("x is no dispersal kernel")
+#   }
+#   p <- x$prep_dk
+#   if (is.null(x$other_vars)) {
+#     p$other_covars <- matrix(NA, nrow = n, ncol = 1)
+#   }
+#
+#   k <- simulate_track(
+#     cur_x = p$start[1], cur_y = p$start[2], nc = p$nc, nr = p$nr,
+#     dk = p$dk, coefs = p$coefs,
+#     standardize = p$standardize,
+#     first_order_terms = p$first_order_terms,
+#     second_order_terms = p$second_order_terms,
+#     hab = p$hab,
+#     other_covars = p$other_covars,
+#     other_covars_indicator = p$other_covars_indicator,
+#     stop = p$stop, n = n
+#   )
+#
+#   tibble::tibble(
+#     x1 = k[, 1] * p$res,
+#     y1 = k[, 2] * p$res,
+#     x2 = k[, 3] * p$res,
+#     y2 = k[, 4] * p$res,
+#     ta_ = k[, 5],
+#     sl_ = k[, 6] * p$res
+#   )
+#
+# }
+#
+#  simulate_xy_R <- function(obj, n = 100, other_vars = NULL) {
+#
+#    if (!is(obj, "dispersal_kernel")) {
+#      stop("obj is no dispersal kernel")
+#    }
+#    p <- obj$prep_dk
+#    if (is.null(obj$other_vars)) {
+#      p$other_covars <- matrix(NA, nrow = n, ncol = 1)
+#    }
+#
+#    x <- p$start[1]
+#    y <- p$start[2]
+#
+#    out <- data.frame(
+#      x1 = rep(NA, n),
+#      y1 = NA,
+#      x2 = NA,
+#      y2 = NA,
+#      sl_ = NA,
+#      ta_ = NA
+#    )
+#
+#    dk <- p$dk
+#
+#    for (i in 1:n) {
+#
+#      out$x1[i] <- x
+#      out$y1[i] <- y
+#
+#      k <- dispersal_kernel(
+#        cur_x = x, cur_y = y, nc = p$nc, nr = p$nr,
+#        dk = p$dk, coefs = p$coefs,
+#        standardize = p$standardize,
+#        first_order_terms = p$first_order_terms,
+#        second_order_terms = p$second_order_terms,
+#        hab = p$hab,
+#        other_covars = p$other_covars,
+#        other_covars_indicator = p$other_covars_indicator,
+#        stop = p$stop
+#      )
+#
+#      next_pixel <- sample.int(nrow(p$dk), size = 1, prob = k[, 3])
+#      x <- k[next_pixel, 1]
+#      y <- k[next_pixel, 2]
+#
+#      ###
+#      #plot(rasterFromXYZ(k))
+#      #points(out[i, "x1"], out[i, "y1"])
+#      #points(x, y)
+#
+#
+#      atan2_north <- function(y, x) {
+#        ta <- atan2(y, x)
+#        ta <- ta - (pi/2)
+#        ta <- ifelse(ta < -pi, ta + 2 * pi, ta)
+#        ifelse(ta > pi, ta - 2 * pi, ta)
+#      }
+#
+#      ta <- atan2_north(y -  out[i, "y1"], x - out[i, "x1"])
+#      dk[, "cos_ta_"] <- get_angle(dk[, 1:2], dir = ta, FALSE)
+#      # plot(rasterFromXYZ(dk[, c(1:2, 5)]))
+#      # head(dk)
+#      ###
+#
+#      out$x2[i] <- x
+#      out$y2[i] <- y
+#
+#      out$sl_[i] <- p$dk[next_pixel, "sl_"]
+#      out$ta_[i] <- ta
+#    }
+#    out
+#  }
+#
--- src/simulate2.cpp
+++ src/simulate2.cpp
@@ -3,12 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <math.h>
+
+
 
 /* name spaces */
 using namespace Rcpp;
 using namespace std;
 
 // Thanks: http://stackoverflow.com/questions/4003232/how-to-code-a-modulo-operator-in-c-c-obj-c-that-handles-negative-numbers
+//[[Rcpp::export]]
 int mod (int a, int b) {
   if(b < 0) //you can check for b == 0 separately and do what you want
     return mod(-a, -b);
@@ -27,101 +31,368 @@ double frand(double upper) {
 
 
 // http://stackoverflow.com/questions/1761626/weighted-random-numbers
-int rsamp(vector<int>& nums, vector<double>& probs) {
-
+int rsamp(int num, NumericVector probs) {
   double sum_of_weight = 0, rnd;
-  vector<int>::iterator it_num;
-  vector<double>::iterator it_prob;
 
   // sum weights
-  for (it_prob = probs.begin(); it_prob < probs.end(); it_prob++) {
-    sum_of_weight += *it_prob;
+  for (int i = 0; i < probs.length(); i++) {
+    sum_of_weight += probs[i];
   }
 
   rnd = frand(sum_of_weight);
 
-  for(it_prob = probs.begin(), it_num = nums.begin(); it_prob < probs.end(); it_prob++, it_num++) {
-    if(rnd < *it_prob)
-      return *it_num;
-    rnd -= *it_prob;
+  for(int i = 0; i < num; i++) {
+    if(rnd < probs[i])
+      return i;
+    rnd -= probs[i];
   }
   return 0;
 }
 
-// Simulate UD new try
+
 //[[Rcpp::export]]
+NumericMatrix dispersal_kernel  (
+    int cur_x, int cur_y, int nc, int nr,
+    NumericMatrix dk, NumericVector coefs,
+    int standardize,
+    IntegerVector first_order_terms,
+    IntegerVector second_order_terms,
+    NumericMatrix hab,
+    NumericMatrix other_covars,
+    IntegerVector other_covars_indicator,
+    int stop
+) {
+  int p_hab = 0, p_other = 0, p = 0, npot, cell, cell_cur, new_x, new_y;
 
-NumericMatrix simulate_2(
-    int n_steps, int start, int nc, int nr,
-    NumericMatrix hab, NumericMatrix dk) {
-  // hab: the habitat matrix of dimension: n * p
-  //    - n: number of cells
-  //    - p: number of predictors
-  // dk: the offset of each cell in the movement kernel to the current cell. Dimension: k * 2
-  //    - k: number of cells in the dispersal kernel
-  //    - columns: offset in x and y direction
+  if (other_covars_indicator[0] == 1) {
+    p_hab = hab.ncol();
+  }
 
-  // Todo: - non spatial covariates (that are the same for each step, e.g., time of day)
+  if (other_covars_indicator[1] == 1) {
+    p_other = other_covars.ncol();
+  }
 
-  int npot = dk.nrow(); // number of options cells possible move to (-> # cells in the movement kernel)
+  // p_hab is used twice, because we store beginning and end of step
+  p = 3 + p_hab + p_hab + p_other; // sl, log_l, ta + env * 2 (start and end) + (other)
+  npot = dk.nrow();
+  NumericMatrix design_matrix(npot, p);
+  NumericMatrix res(npot, 3);
+  NumericMatrix stop_return(1,1);
+  stop_return(0,0) = -9999;
 
-  IntegerVector cells (npot);  // create vector of pointers
-  NumericVector probs (npot);
+  int cur_cell = cur_x + cur_y * nc;
 
-  // define output here
-  // IntegerVector ud(hk.nrow(), 0);
+  // loop over all positions in the dispersal kernel
+  for (int k = 0; k < npot; k++) {
+    // The possilbe cells are: the current cell + cells that fall within the movement
+    // kernel
 
-  int p = start; // current position
-  // ud(k)++;
+    // row-major order
+    // https://stackoverflow.com/questions/5991837/row-major-order-indices
+    // index = X + Y * Width
+    // Y = (int)(index / Width)
+    // X = index - (Y * Width)
 
-  // step is just repeating the simulations n_steps time
-  // and not really neeeded in the simulations
+    // to get cell, we use: x % nr + nc * y / nc
+    // nc * (y - 1) + x
+    // x = k mod nc
+    // y = k / nr
 
-  // The variable k holds the current cell number
-  for (int step = 1; step < n_steps; step++) {
-    for (int k = 0; k < npot; k++) {
+    // (p % nc): gives the x coord of the current cell
+    // dk(k, 0): is the offset of cell in the dispersal kernel in x direction
+    // mod(....., nc): translates the x-part back to the cell number
 
-      // The possilbe cells are: the current cell + cells that fall within the movement
-      // kernel
+    // (p / nc): gives the y coord of the current cell
+    // dk(k, 1): is the offset in y direction of the dispersal kernel
+    // nc * mod(...., nr) translates the y-part back to the cell number
 
-      // row-major order
-      // https://stackoverflow.com/questions/5991837/row-major-order-indices
-      // index = X + Y * Width
-      // Y = (int)(index / Width)
-      // X = index - (Y * Width)
-
-      // to get cell, we use: x % nr + nc * y / nc
-      // nc * (y - 1) + x
-      // x = k mod nc
-      // y = k / nr
-
-      // (p % nc): gives the x coord of the current cell
-      // dk(k, 0): is the offset of cell in the dispersal kernel in x direction
-      // mod(....., nc): translates the x-part back to the cell number
-
-      // (p / nc): gives the y coord of the current cell
-      // dk(k, 1): is the offset in y direction of the dispersal kernel
-      // nc * mod(...., nr) translates the y-part back to the cell number
+    new_x = cur_x + dk(k, 0);
+    new_y = cur_y + dk(k, 1);
 
 
-      cells[k] = mod((p % nc) + dk(k, 0), nc) + nc * mod(((p / nr) + dk(k, 1)), nr);
-
-      // now calculate for each cell the probability of moving there
-      probs[k] = 0;
-      for (int j = 0; j < p; j++) {
-        probs[k] += coef[j] * hab[cells[k], j];
+    // check if we stepped out of the world
+    if (new_x < 0) {
+      if (stop == 1) {
+        return stop_return;
+      } else {
+        new_x = mod(new_x, nc);
       }
-      // probs[k] = hk(cells[k][j], 1) * mk(j, 2);
+    }
+
+    if (new_y < 0) {
+      if (stop == 1) {
+        return stop_return;
+      } else {
+        new_y = mod(new_y, nr);
+      }
+    }
+
+    if (new_x > nc) {
+      if (stop == 1) {
+        return stop_return;
+      } else {
+        new_x = mod(new_x, nc);
+      }
+    }
+
+    if (new_y >= nr) {
+      if (stop == 1) {
+        return stop_return; }
+      else {
+        new_y = mod(new_y, nr);
+      }
+    }
+
+    res(k, 0) = new_x;  // x of the dispersal kernel
+    res(k, 1) = new_y;  // y of the dispersal kernel
+
+    // The cell of the potential position
+    cell = new_x + new_y * nc;
+
+    // prepare design matrix
+    design_matrix(k, 0) = dk(k, 2); // sl_
+    design_matrix(k, 1) = dk(k, 3); // log_sl_
+    design_matrix(k, 2) = dk(k, 4); // cos of ta
+
+    // start
+    int col_counter = 3;
+    if (p_hab > 0) {
+      for (int i = 0; i < p_hab; i++) {
+        design_matrix(k, col_counter) = hab(cur_cell, i); // value at current cell (start)
+        col_counter++;
+        design_matrix(k, col_counter) = hab(cell, i); // value at potential cell (end)
+        col_counter++;
+      }
+    }
+
+    // other
+    if (p_other > 0) {
+      for (int i = 0; i < p_other; i++) {
+        design_matrix(k, col_counter) = other_covars(0, i);
+        col_counter++;
+      }
     }
   }
-  //k = rsamp(cells[k], probs[k]);
-  //ud(k)++;
 
-  NumericMatrix out(npot, 2);
+  // Multiply design matrix with coefficients
+  double sel;
   for (int i = 0; i < npot; i++) {
-    out(i,0) = cells[i] % nc;
-    out(i,1) = cells[i] / nr;
+    for (int j = 0; j < first_order_terms.length(); j++) {
+      if (second_order_terms[j] != -1) {
+        sel = design_matrix(i, first_order_terms[j]) * design_matrix(i, second_order_terms[j]);
+      } else {
+        sel = design_matrix(i, first_order_terms[j]) ;
+      }
+      res(i, 2) += sel * coefs[j]; // Matrix multiplication is done here already
+    }
   }
 
-  return out;
+  // Exponiate here
+  res(_, 2) = exp(res(_, 2));
+
+  // Standardize return
+  if (standardize == 1) {
+    res(_, 2) = res(_, 2) / sum(na_omit(res(_, 2)));
+  }
+  return res;
 }
+
+// Calcualte atan2, but 0 is north
+//[[Rcpp::export]]
+double atan2_north_cpp(double y, double x) {
+  double ta = atan2(y, x) - (M_PI / 2);
+  if (ta < -M_PI) {
+    ta = ta + 2 * M_PI;
+  }
+  if (ta > M_PI) {
+    ta = ta - 2 * M_PI;
+  }
+  return ta;
+}
+
+
+//[[Rcpp::export]]
+NumericVector get_angle_cpp(NumericMatrix xy, double dir = 0) {
+  int n = xy.nrow();
+  NumericVector res(n);
+  double tmp;
+
+  for (int i = 0; i < n; i++) {
+    tmp = atan2(xy(i, 1), xy(i, 0)) - (M_PI/2) + dir;
+    if (tmp < -M_PI) {
+      tmp = tmp + 2 * M_PI;
+    }
+    if (tmp > M_PI) {
+      tmp = tmp - 2 * M_PI;
+    }
+    res[i] = cos(tmp);
+  }
+  return res;
+}
+
+
+double cos_t(double x) {
+  int fac2 = 2;
+  int fac4 = 24;
+  int fac6 = 720;
+  int fac8 = 40320;
+
+  double xp2 = x * x;
+  double xp4 = xp2 * xp2;
+  double xp6 = xp2 * xp4;
+  double xp8 = xp4 * xp4;
+
+  return 1 - xp2 / fac2 + xp4 / fac4 - xp6 / fac6 + xp8 / fac8;
+
+}
+
+//[[Rcpp::export]]
+NumericVector get_angle_cpp1(NumericMatrix xy, double dir = 0) {
+  int n = xy.nrow();
+  NumericVector res(n);
+  double tmp;
+
+  for (int i = 0; i < n; i++) {
+    tmp = atan2(xy(i, 1), xy(i, 0)) - (M_PI/2) + dir;
+
+    if (tmp < -M_PI) {
+      tmp = tmp + 2 * M_PI;
+    }
+    if (tmp > M_PI) {
+      tmp = tmp - 2 * M_PI;
+    }
+
+    res[i] = cos_t(tmp);
+  }
+  return res;
+}
+
+// https://gist.github.com/volkansalma/2972237
+float atan2_approximation1(float y, float x)
+{
+  //http://pubs.opengroup.org/onlinepubs/009695399/functions/atan2.html
+  //Volkan SALMA
+
+  const float ONEQTR_PI = M_PI / 4.0;
+  const float THRQTR_PI = 3.0 * M_PI / 4.0;
+  float r, angle;
+  float abs_y = fabs(y) + 1e-10f;      // kludge to prevent 0/0 condition
+  if ( x < 0.0f )
+  {
+    r = (x + abs_y) / (abs_y - x);
+    angle = THRQTR_PI;
+  }
+  else
+  {
+    r = (x - abs_y) / (x + abs_y);
+    angle = ONEQTR_PI;
+  }
+  angle += (0.1963f * r * r - 0.9817f) * r;
+  if ( y < 0.0f )
+    return( -angle );     // negate if in quad III or IV
+  else
+    return( angle );
+
+
+}
+
+//[[Rcpp::export]]
+NumericVector get_angle_cpp2(NumericMatrix xy, double dir = 0) {
+  int n = xy.nrow();
+  NumericVector res(n);
+  double tmp;
+
+  for (int i = 0; i < n; i++) {
+    tmp = atan2_approximation1(xy(i, 1), xy(i, 0)) - (M_PI/2) + dir;
+
+    if (tmp < -M_PI) {
+      tmp = tmp + 2 * M_PI;
+    }
+    if (tmp > M_PI) {
+      tmp = tmp - 2 * M_PI;
+    }
+
+    res[i] = cos_t(tmp);
+  }
+  return res;
+}
+
+// This not funcitonal at the moment
+/*
+ NumericMatrix simulate_track (
+     int cur_x, int cur_y, int nc, int nr,
+     NumericMatrix dk, NumericVector coefs,
+     int standardize,
+     IntegerVector first_order_terms,
+     IntegerVector second_order_terms,
+     NumericMatrix hab,
+     NumericMatrix other_covars,
+     IntegerVector other_covars_indicator,
+     int stop, int n
+ ) {
+
+   /*
+    * res: is the result matrix
+    * The columns are:
+    *  - x1
+    *  - y1
+    *  - x2
+    *  - y2
+    *  - sl_
+    *  - ta_
+
+   NumericMatrix res(n, 6);
+   NumericMatrix this_other_covar (1, other_covars.ncol());
+   int x = cur_x;
+   int y = cur_y;
+   double dir;
+
+   // dispersal kernel
+   int nc_dk = dk.nrow();
+   NumericMatrix cells(nc_dk, 3);
+
+   for (int i = 0; i < n; i++) {
+
+     this_other_covar(0, _) = other_covars(i,_);
+
+     // Save start x and y
+     res(i, 0) = x;
+     res(i, 1) = y;
+
+     cells = dispersal_kernel(x, y, nc, nr,
+       dk, coefs,
+       standardize,
+       first_order_terms,
+       second_order_terms,
+       hab,
+       this_other_covar,
+       other_covars_indicator, stop);
+
+     // Stop if outside the landscape
+     if (cells(0,2) == -9999) {
+       return res;
+     }
+
+    // Sample one cell
+    int wc = rsamp(nc_dk, cells(_,2));
+
+    // Update x and y
+    x = cells(wc, 0);
+    y = cells(wc, 1);
+
+    dir = atan2_north_cpp(y - res(i, 1), x - res(i, 0));
+    dk(_,4) = get_angle_cpp2(dk(_, Range(0, 1)));
+
+    // Save updated x and y
+    res(i, 2) = x;
+    res(i, 3) = y;
+
+    // Save dir and sl_
+    res(i, 4) = dir;
+    res(i, 5) = dk(wc, 2);
+   }
+   return res;
+ }
+
+
+*/
