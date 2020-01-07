008d196e9d8ba294d5d7617a766d417660942f1f
jmsigner <jmsigner@gmail.com>
2020-01-07 16:02:23 +0100

    prep update

--- tests/testthat/test.fitdistr.R
+++ tests/testthat/test.fitdistr.R
@@ -1,10 +1,10 @@
 data(deer)
-d1 <- deer %>% steps_by_burst()
+d1 <- deer %>% steps()
 d2 <- deer %>% steps_by_burst() %>% mutate(a = 1, b = 2) %>%
   random_steps(ta_distr = "vonmises", sl_dist = "gamma")
 d3 <- d2 %>% fit_clogit(case_ ~ sl_ + strata(step_id_))
 
-test_that("fit_distr for ta and sl work", {
+testthat::test_that("fit_distr for ta and sl work", {
   expect_equal(sl_distr(d2), "gamma")
   expect_equal(ta_distr(d2), "vonmises")
 
