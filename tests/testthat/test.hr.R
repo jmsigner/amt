abc70e05df8dbba815ad493acacaa8a47b153b0a
jmsigner <jmsigner@gmail.com>
2020-01-16 09:54:48 +0100

    added akde tests

--- tests/testthat/test.hr.R
+++ tests/testthat/test.hr.R
@@ -4,7 +4,7 @@ set.seed(123)
 mini_fisher <- amt_fisher[sample(nrow(amt_fisher), 150), ]
 
 
-test_that("Create steps", {
+test_that("MPC and KDE have the right class", {
   expect_is(mini_fisher %>% hr_mcp, "mcp")
   expect_is(mini_fisher %>% hr_mcp, "hr")
   expect_is(mini_fisher %>% hr_kde, "kde")
@@ -12,4 +12,13 @@ test_that("Create steps", {
   expect_is(mini_fisher %>% hr_locoh, "locoh")
   expect_is(mini_fisher %>% hr_locoh, "hr")
 })
+class(mini_fisher %>% hr_akde)
 
+test_that("akde works", {
+  expect_is(mini_fisher %>% hr_akde, "akde")
+  expect_is(mini_fisher %>% hr_akde, "hr")
+  expect_warning(
+    expect_error(mini_fisher %>% hr_akde(., model = fit_ctmm(., "bm")))
+  )
+
+})
