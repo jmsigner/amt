dc07c6874667fe7b3807255437daae89e3017618
Johannes Signer <jmsigner@gmail.com>
2020-05-19 10:49:51 +0200

    as_track for `steps`

--- R/as_track.R
+++ R/as_track.R
@@ -28,3 +28,30 @@ as_track.sfc_POINT <- function(x, ...) {
   track(x = xx[, 1], y = xx[, 2], crs = sf::st_crs(x))
 }
 
+
+#' @export
+#' @rdname as_track
+as_track.steps_xyt <- function(x, ...) {
+  n <- nrow(x)
+
+  if ("burst_" %in% names(x)) {
+    xx <- tidyr::nest(x, data = -c(burst_)) %>%
+      dplyr::mutate(data = purrr::map(data, function(y) {
+        n1 <- nrow(y)
+        tibble::tibble(
+          xs = c(y$x1_, y$x2_[n1]),
+          ys = c(y$y1_, y$y2_[n1]),
+          t = c(y$t1_, y$t2_[n1])
+        )
+      })) %>% tidyr::unnest(cols = data)
+    make_track(xx, xs, ys, t, burst_ = burst_, crs = get_crs(x))
+  } else {
+    xx <- tibble::tibble(
+      xs = c(x$x1_, x$x2_[n]),
+      ys = c(x$y1_, x$y2_[n]),
+      t = c(x$t1_, x$t2_[n])
+    )
+    make_track(xx, xs, ys, t, crs = get_crs(x))
+  }
+
+}
