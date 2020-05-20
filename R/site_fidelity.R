43b5173e0390a4e56c33b59684041d7425ec4396
Johannes Signer <jmsigner@gmail.com>
2020-05-20 14:18:36 +0200

    site fidelity

--- R/site_fidelity.R
+++ R/site_fidelity.R
@@ -6,67 +6,65 @@
 #' @param n Numeric scalar. The number of simulated trajectories.
 #' @param alpha Numeric scalar. The alpha value used for the bootstrapping.
 #' @export
-#' @return A list of length 4. `msd.dat` and `li.dat`` is the mean square distance and linearity for the real date. `msd.sim` and `li.sim`` are the mean square distances and linearities for the simulated trajectories.
+#' @return A list of length 4. `msd_dat` and `li_dat` is the mean square distance and linearity for the real date. `msd_sim` and `li_sim`` are the mean square distances and linearities for the simulated trajectories.
 #'
 #' @references Spencer, S. R., Cameron, G. N., & Swihart, R. K. (1990). Operationally defining home range: temporal dependence exhibited by hispid cotton rats. Ecology, 1817-1822.
 #' @examples
-#' # Simulate a random walk.
-#' set.seed(123)
-#' a <- rhrRW(n = 1000)
+#' # real data
 #' \dontrun{
-#' plot(a)
-#' }
-#'
-#' # Calcualte site fidelity
-#' sf <- rhrSiteFidelity(a, n = 200)
-#'
-#' # For MSD and LI the observed data do not differ significantely from random permutations.
-#' \dontrun{
-#' plot(sf)
-#' }
-#'
-#' # Simulate trajectory as Ornstein-Uhlenbeck process
-#' a <- rhrOU(n = 10000, A = matrix(c(0.1, 0, 0, 0.1), 2))
-#' plot(a)
-#' sf <- rhrSiteFidelity(a, n = 200)
-#'
-#' # For MSD and LI the observed data differ significantely from random permutations.
-#' \dontrun{plot(sf)}
-#'
-#' ## real data
-#' data(datSH)
-#' res <- rhrSiteFidelity(datSH[, 2:3], n = 200)
-#' \dontrun{
-#' plot(res)
+#' data(deer)
+#' ds <- deer %>% steps_by_burst()
+#' site_fidelity(ds)
 #' }
 
-site_fidelty <- function(x, n = 100, alpha = 0.05) {
+site_fidelty <- function(x, ...) {
+  UseMethod("site_fidelity", x)
+}
+
+
+#' @export
+site_fidelty.steps_xy <- function(x, n = 100, alpha = 0.05, ...) {
+
+  ##
+  library(amt)
+  data(deer)
+  n <- 100
+  x <- steps(deer)
+  alpha <- 0.05
+  ##
 
 
   # Some argument checking
   checkmate::assert_numeric(n, lower = 1, len = 1)
   checkmate::assert_numeric(alpha, lower = 0, upper = 1, len = 1)
 
+
+
   ## simulate n random walks
-  a <- replicate(n, rhrPRW(x, y), simplify=FALSE)
+  a <- replicate(n, permute_steps(x), simplify=FALSE)
 
   ## msd
-  msdDat <- msd(x, y)
-  msdSim <- sapply(a, function(x) rhrMSD2(x[, 1], x[, 2]))
+  msd_dat <- msd(x)
+  msd_sim <- sapply(a, msd)
 
   ## li
-  liDat <- li(x, y)
-  liSim <- sapply(a, function(x) li(x[,1], x[,2]))
+  li_dat <- li(x)
+  li_sim <- sapply(a, li)
 
   ## CI
-  msdCI <- quantile(msdSim, probs=c(alpha / 2, 1 - alpha / 2))
-  liCI <- quantile(liSim, probs=c(alpha / 2, 1 - alpha / 2))
+  msd_ci <- quantile(msd_sim, probs=c(alpha / 2, 1 - alpha / 2))
+  li_ci <- quantile(li_sim, probs=c(alpha / 2, 1 - alpha / 2))
 
 
-  res <- list(msdDat=msdDat, liDat=liDat, msdSim=msdSim, liSim=liSim,
-              msdCI=msdCI, liCI=liCI)
-
-  res <- structure(res, class=c("RhrSiteFidelity", "list"))
+  res <-
+    list(
+      msd_dat = msd_dat,
+      li_dat = li_dat,
+      msd_sim = msd_sim,
+      li_sim = li_sim,
+      msd_ci = msd_ci,
+      li_ci = li_ci
+    )
   invisible(res)
 }
 
@@ -90,16 +88,20 @@ permute_steps <- function(x) {
   d <- sample(x$sl_)
   n <- length(d)
   a <- runif(n, 0, 2 * pi)
-
   sinrA <- sin(a)
   cosrA <- cos(a)
 
-  res <- matrix(nrow = n, ncol = 2)
-  res[1, ] <- c(x$x1_[1], x$y1_[1])
+  res <- data.frame(x1_ = NA, y1_ = NA, x2_ = NA, y2_ = NA, sl_ = NA)
+  xs <- x$x1_[1]
+  ys <- x$y1_[1]
 
-  for (i in 1:(n-1)) {
-    res[i + 1, 1] = res[i, 1] + cosrA[i] * d[i]
-    res[i + 1, 2] = res[i, 2] + sinrA[i] * d[i]
+  # in steps
+  for (i in 1:n) {
+    xe <- xs + cosrA[i] * d[i]
+    ye <- ys + sinrA[i] * d[i]
+    res[i, ] <- list(xs, ys, xe, ye, d[i])
+    xs <- xe
+    ys <- ye
   }
   res
 }
