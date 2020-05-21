c1770b703aba0aa5e50ba1c6af1ae4012ef9eb1e
Johannes Signer <jmsigner@gmail.com>
2020-05-21 07:44:57 +0200

    updated hr_plot

--- R/hr_plot.R
+++ R/hr_plot.R
@@ -1,5 +1,8 @@
 #' @export
-plot.hr <- function(x, col = NA, ...) {
-  plot(hr_isopleths(x)[, "level"], col = col, ...)
-
+plot.hr <- function(x, ...) {
+    plot(sf::st_geometry(hr_isopleths(x)), ...)
+  if (!is.null(x$data)) {
+    points(x$data$x_, x$data$y_, col = grDevices::adjustcolor("black", 0.2),
+           pch = 20)
+  }
 }
