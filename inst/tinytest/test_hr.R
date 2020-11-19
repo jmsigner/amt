06956be9ad06872c09c1e745e92f22392ec37a8a
Johannes Signer <jmsigner@gmail.com>
2020-11-19 09:50:48 +0100

    more test

--- inst/tinytest/test_hr.R
+++ inst/tinytest/test_hr.R
@@ -1,29 +1,8 @@
 library(amt)
 data(amt_fisher)
 set.seed(123)
-mini_fisher <- amt_fisher[sample(nrow(amt_fisher), 150), ]
 
-
-# MCP and KDE have the right class
-
-expect_true(is(mini_fisher %>% hr_mcp, "mcp"))
-expect_true(is(mini_fisher %>% hr_mcp, "hr"))
-expect_true(is(mini_fisher %>% hr_kde, "kde"))
-expect_true(is(mini_fisher %>% hr_kde, "hr"))
-expect_true(is(mini_fisher %>% hr_locoh, "locoh"))
-expect_true(is(mini_fisher %>% hr_locoh, "hr"))
-expect_true(is(mini_fisher %>% hr_akde, "akde"))
-expect_true(is(mini_fisher %>% hr_akde, "hr"))
-
-# More tests for mcp
-tinytest::expect_error(hr_mcp(mini_fisher, keep.data = c(TRUE, FALSE)))
-tinytest::expect_error(hr_mcp(mini_fisher, keep.data = mini_fisher))
-tinytest::expect_true(is(hr_mcp(mini_fisher, keep.data = TRUE)$data, "track_xy"))
-tinytest::expect_true(is.null(hr_mcp(mini_fisher, keep.data = FALSE)$data))
-
-# Test home-range model
 mini_fisher <- amt_fisher[1:50, ]
-
 mcp <- hr_mcp(mini_fisher)
 loc <- hr_locoh(mini_fisher)
 kde <- hr_kde(mini_fisher)
@@ -31,6 +10,25 @@ kde <- hr_kde(mini_fisher)
 m <- fit_ctmm(mini_fisher, "iid")
 rd <- hr_akde(mini_fisher, model = m)
 od <- hr_od(mini_fisher, model = m)
+
+# MCP and KDE have the right class
+
+expect_true(is(mcp, "mcp"))
+expect_true(is(mcp, "hr"))
+expect_true(is(kde, "kde"))
+expect_true(is(kde, "hr"))
+expect_true(is(loc, "locoh"))
+expect_true(is(loc, "hr"))
+expect_true(is(rd, "akde"))
+expect_true(is(rd, "hr"))
+
+# More tests for mcp
+tinytest::expect_error(hr_mcp(mini_fisher, keep.data = c(TRUE, FALSE)))
+tinytest::expect_error(hr_mcp(mini_fisher, keep.data = mini_fisher))
+tinytest::expect_true(is(mcp$data, "track_xy"))
+tinytest::expect_true(is.null(hr_mcp(mini_fisher, keep.data = FALSE)$data))
+
+# Test home-range model
 
 # keep.data
 tinytest::expect_true(is(mcp$data, "track_xyt"))
@@ -73,3 +71,15 @@ tinytest::expect_equal(hr_overlap(loc, loc), 1)
 tinytest::expect_equal(hr_overlap(kde, kde), 1)
 tinytest::expect_equal(hr_overlap(od, od), 1)
 tinytest::expect_equal(hr_overlap(od, od), 1)
+
+tinytest::expect_true(all(names(hr_area(rd)) == c("level", "what", "area")))
+tinytest::expect_true(all(names(hr_area(loc)) == c("level", "what", "area")))
+tinytest::expect_true(all(names(hr_area(mcp)) == c("level", "what", "area")))
+tinytest::expect_true(all(names(hr_area(kde)) == c("level", "what", "area")))
+tinytest::expect_true(all(names(hr_isopleths(rd)) == c("level", "what", "area", "geometry")))
+tinytest::expect_true(all(names(hr_isopleths(loc)) == c("level", "what", "area", "geometry")))
+tinytest::expect_true(all(names(hr_isopleths(mcp)) == c("level", "what", "area", "geometry")))
+tinytest::expect_true(all(names(hr_isopleths(kde)) == c("level", "what", "area", "geometry")))
+
+hr_isopleths(od)
+class(od)
