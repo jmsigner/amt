1c0c34b863d7a827886db2925b247d9873366c3f
Johannes Signer <jmsigner@gmail.com>
2021-01-12 06:10:47 +0100

    improved tests

--- inst/tinytest/test_as_telemetry.R
+++ inst/tinytest/test_as_telemetry.R
@@ -7,9 +7,9 @@ dat <- tibble(x = 1:10, y = 1:10, t = ymd_hms("2020-11-18 00:00:00") + hours(0:9
 
 tr <- make_track(dat, x, y, t, all_cols = TRUE, crs = CRS("+init=epsg:4326"))
 
-tinytest::expect_true("HDOP" %in% names(as_telemetry(select(tr, x_, y_, t_, dop))))
-tinytest::expect_true("HDOP" %in% names(as_telemetry(select(tr, x_, y_, t_, hdop))))
-tinytest::expect_true("HDOP" %in% names(as_telemetry(select(tr, x_, y_, t_, DOP))))
-tinytest::expect_true("HDOP" %in% names(as_telemetry(select(tr, x_, y_, t_, HDOP))))
-tinytest::expect_false("HDOP" %in% names(as_telemetry(select(tr, x_, y_, t_))))
+expect_true("HDOP" %in% names(as_telemetry(amt::select(tr, x_, y_, t_, dop))))
+expect_true("HDOP" %in% names(as_telemetry(amt::select(tr, x_, y_, t_, hdop))))
+expect_true("HDOP" %in% names(as_telemetry(amt::select(tr, x_, y_, t_, DOP))))
+expect_true("HDOP" %in% names(as_telemetry(amt::select(tr, x_, y_, t_, HDOP))))
+expect_false("HDOP" %in% names(as_telemetry(amt::select(tr, x_, y_, t_))))
 
