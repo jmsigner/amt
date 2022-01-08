6c437c902741e8103e3578b645d3537c270f4070
Johannes <jmsigner@gmail.com>
2022-01-08 07:28:40 +0100

    New method `add_nsd()` to calculate the NSD for tracks and steps.

--- R/nsd.R
+++ R/nsd.R
@@ -1,16 +1,38 @@
 #' Net squared displacement (nsd)
 #'
-#' Calculate the net squared displacement (i.e., the squared distance from the first location of a track) for a track.
+#' The function `nsd()` calculates the net squared displacement (i.e., the squared distance from the first location of a track) for a track. The function `add_nsd()` add a new column to a track or steps object with the nsd (the column name is `nsd_`).
 #'
 #' @param x A track_xy{t}.
 #' @template dots_none
-#' @return Numeric vector.
+#' @return Numeric vector (for `nsd()`) and a tillbe with the original data with a new column for `add_nsd()`.
+#' @name nsd
 #' @export
 nsd <- function(x, ...) {
   UseMethod("nsd", x)
 }
 
 #' @export
+#' @rdname nsd
 nsd.track_xy <- function(x, ...) {
   (x$x_ - x$x_[1])^2 + (x$y_ - x$y_[1])^2
+}
+
+#' @export
+#' @rdname nsd
+add_nsd <- function(x, ...) {
+  UseMethod("add_nsd", x)
+}
+
+#' @export
+#' @rdname nsd
+add_nsd.track_xy <- function(x, ...) {
+  x[["nsd_"]] <- nsd(x)
+  x
+}
+
+#' @export
+#' @rdname nsd
+add_nsd.steps_xy <- function(x, ...) {
+  x[["nsd_"]] <- (x$x2_ - x$x1_[1])^2 + (x$y2_ - x$y1_[1])^2
+  x
 }
---
+++ inst/tinytest/test_nsd.R
@@ -0,0 +1,17 @@
+data(deer)
+
+nsd0 <- deer %>% nsd()
+nsd1 <- deer %>% add_nsd()
+nsd2 <- deer %>% steps() %>% add_nsd()
+
+all(n1 == n2[-1])
+
+
+expect_true(is(nsd0, "numeric"))
+expect_true(is(nsd1, "data.frame"))
+expect_true(is(nsd2, "data.frame"))
+expect_true(is(nsd1, "track_xyt"))
+expect_true(is(nsd2, "steps_xyt"))
+
+expect_true(all(nsd0 == nsd1$nsd_))
+expect_true(all(nsd0[-1] == nsd2$nsd_))
