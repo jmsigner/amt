c0f8f3f5fe39d83cf3c7cdfca5912a67d1dc2c50
bsmity13 <bsmity13@gmail.com>
2022-03-04 17:35:08 -0700

    Finished first draft of uhc_prep() function.

--- R/uhc_plots.R
+++ R/uhc_plots.R
@@ -45,7 +45,7 @@ prep_uhc <- function(object, test_dat, n_samp = 1000, verbose = TRUE) {
   ## object
   if(!inherits(object, c("glm", "fit_logit", "fit_clogit"))){
     stop(paste0("'object' should be an object of class 'glm', 'fit_logit', ",
-    "or 'fit_clogit'."))
+                "or 'fit_clogit'."))
   }
 
   ## test_dat
@@ -60,7 +60,7 @@ prep_uhc <- function(object, test_dat, n_samp = 1000, verbose = TRUE) {
 
 #' @rdname prep_uhc
 #' @export
-prep_uhc.glm <- function(object, test_dat, n_samp, verbose = TRUE) {
+prep_uhc.glm <- function(object, test_dat, n_samp = 1000, verbose = TRUE) {
 
   # Prep test_dat list
   l <- prep_test_dat(object, test_dat, verbose = verbose)
@@ -68,28 +68,9 @@ prep_uhc.glm <- function(object, test_dat, n_samp, verbose = TRUE) {
   # Steps follow steps in Fieberg et al. 2018
 
   # Step 1. Summarize distribution of covariates
-  dist_dat <- lapply(l$vars, function(v){
-    u <- l$data[[v]][which(l$data[[l$resp]] == 1)]
-    a <- l$data[[v]][which(l$data[[l$resp]] == 0)]
-
-    # Decide whether 'v' is numeric
-    if (l$type[[v]] == "numeric") {
-      f_u <- stats::density(u)
-      f_u_df <- data.frame(x = f_u$x, y = f_u$y, dist = "U")
-      f_a <- stats::density(a)
-      f_a_df <- data.frame(x = f_a$x, y = f_a$y, dist = "A")
-      df <- rbind(f_u_df, f_a_df)
-    } else {
-      f_u <- table(u)/length(u)
-      f_u_df <- data.frame(x = names(f_u), y = as.numeric(f_u), dist = "U")
-      f_a <- table(a)/length(a)
-      f_a_df <- data.frame(x = names(f_a), y = as.numeric(f_a), dist = "A")
-      df <- rbind(f_u_df, f_a_df)
-      # Restore factor levels
-      df$x <- factor(df$x, levels = levels(l$data[[v]]))
-    }
-
-    return(df)
+  dist_dat <- lapply(1:length(l$vars), function(ii){
+    return(ua_distr(name = l$vars[ii], data = l$data,
+                    type = l$type[ii], resp = l$resp))
   })
 
   names(dist_dat) <- l$vars
@@ -97,16 +78,78 @@ prep_uhc.glm <- function(object, test_dat, n_samp, verbose = TRUE) {
   # Step 2. Fit a model to the training data
   # We've already fit the model, but we need the coefficient estimates and
   # variance-covariance matrix here.
+  b <- coef(object)
+  S <- vcov(object)
 
   # Step 3. Create predicted distribution
+  pred_dist <- lapply(1:n_samp, function(i) {
+    # Report progress
+    if (verbose) {
+      if (i == 1){
+        cat("Sampling...\n")
+      }
+      cat(i, "of", n_samp, "    \r")
+    }
 
-  # Step 3a. Draw random values for the betas
+    ### Step 3a. Draw random values for the betas
+    bb <- MASS::mvrnorm(n = 1, mu = b, Sigma = S)
 
-  # Step 3b. Select points from test data
+    ### Step 3b. Select points from test data
+    # Calculate w(x) under the model
+    ww <- calc_w(f = formula(object), b = bb, newdata = l$data)
 
-  # Step 3c. Summarize covariates at these locations
+    # Normalize for weights
+    WW <- ww/sum(ww)
 
+    # How many points to sample? One for each used point.
+    nn <- sum(l$data[[l$resp]])
 
+    # Sample
+    rr <- sample.int(n = nrow(test_dat), size = nn, replace = TRUE,
+                     prob = WW)
+
+    # Get resulting rows from test_dat
+    dd <- test_dat[rr, ]
+
+    ### Step 3c. Summarize covariates at these locations
+    # All of 'dd' should be treated as 'used' here
+    dd[[l$resp]] <- TRUE
+    cov_dens <- lapply(1:length(l$vars), function(ii){
+      # Summarize distributions
+      xx <- ua_distr(name = l$vars[ii], data = dd,
+                     type = l$type[ii], resp = l$resp,
+                     avail = FALSE)
+      # Label iteration
+      xx$iter <- i
+      # Rearrange
+      yy <- xx[, c("iter", "x", "y")]
+      # Return from covariate lapply
+      return(yy)
+    })
+
+    names(cov_dens) <- l$vars
+
+    # Return from lapply over samples
+    return(cov_dens)
+  })
+
+  # Now rearrange to combine all iterations for each covariate
+  pred_dist_cov <- lapply(l$vars, function(v) {
+    res <- do.call(rbind, lapply(pred_dist, getElement, v))
+    return(res)
+  })
+
+  names(pred_dist_cov) <- l$vars
+
+  # Construct list to return
+  ll <- list(orig = dist_dat,
+             samp = pred_dist_cov)
+
+  # Assign class
+  class(ll) <- c("uhc_data", class(ll))
+
+  # Return
+  return(ll)
 }
 
 #' Prepares `test_dat` for `uch_prep()`
@@ -203,4 +246,78 @@ prep_test_dat.glm <- function(object, test_dat, verbose = TRUE) {
     # Return
     return(l)
   }
+}
+
+#' Summarize distribution of used and available
+#'
+#' Summarizes distribution of numeric or factor variables
+#'
+#' @param name `[character]` Name of the column to summarize.
+#' @param type `[character]` Either `"numeric"` or `"factor"` as returned by
+#' \code{\link{prep_test_dat}()}.
+#' @param data `[data.frame]` The `data.frame` containing the columns and the
+#' response variable.
+#' @param resp `[character]` Name of the response variable.
+#' @param avail `[logical]` Should distribution be calculated for the available
+#' locations? Defaults to `TRUE`, but should be false when summarizing the
+#' bootstrapped "used" samples.
+#'
+ua_distr <- function(name, type, data, resp, avail = TRUE) {
+  u <- data[[name]][which(data[[resp]] == 1)]
+  a <- data[[name]][which(data[[resp]] == 0)]
+
+  # If 'type' is numeric
+  if (type == "numeric") {
+    f_u <- stats::density(u)
+    f_u_df <- data.frame(x = f_u$x, y = f_u$y, dist = "U")
+    if (avail) {
+      f_a <- stats::density(a)
+      f_a_df <- data.frame(x = f_a$x, y = f_a$y, dist = "A")
+      df <- rbind(f_u_df, f_a_df)
+    } else {
+      df <- f_u_df
+    }
+  } else {
+    f_u <- table(u)/length(u)
+    f_u_df <- data.frame(x = names(f_u), y = as.numeric(f_u), dist = "U")
+    if (avail) {
+      f_a <- table(a)/length(a)
+      f_a_df <- data.frame(x = names(f_a), y = as.numeric(f_a), dist = "A")
+      df <- rbind(f_u_df, f_a_df)
+    } else {
+      df <- f_u_df
+    }
+    # Restore factor levels
+    df$x <- factor(df$x, levels = levels(data[[name]]))
+  }
+
+  return(df)
+}
+
+#' Calculate `w(x)`
+#'
+#' Calculates the value of the exponential habitat selection function
+#'
+#' @param f `[formula]` \cr A model formula.
+#' @param b `[numeric]` A named vector of coefficients.
+#' @param newdata `[data.frame]` \cr A `data.frame` to predict eHSF values.
+#'
+calc_w <- function(f, b, newdata) {
+  # Get terms object from formula
+  Terms <- stats::delete.response(terms(f))
+
+  # Create data matrix
+  X <- stats::model.matrix(Terms, data = newdata)
+
+  # Linear combination
+  l <- X %*% b
+
+  # Subtract intercept
+  g <- l[, 1] - b[["(Intercept)"]]
+
+  # Exponentiate
+  w <- exp(g)
+
+  # Return
+  return(w)
 }
